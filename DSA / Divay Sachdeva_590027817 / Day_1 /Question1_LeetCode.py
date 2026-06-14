4ec0c189aeae3f89eba9a936f95a63a692ca03f4
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-14 23:45:45 +0530

    Update and rename Question1_LeetCode.cpp to Question1_LeetCode.py

---
+++ DSA / Divay Sachdeva_590027817 / Day_1 /Question1_LeetCode.py
@@ -0,0 +1,6 @@
+#Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+        return n * (n + 1) // 2 - sum(nums)
