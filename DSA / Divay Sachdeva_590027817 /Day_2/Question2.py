06d1aa24f6f4441d9f8226158eba8fbd1604d484
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-16 12:41:23 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 /Day_2/Question2.py
+++ DSA / Divay Sachdeva_590027817 /Day_2/Question2.py
@@ -1,1 +1,13 @@
+# Given an integer array, determine the largest element present in the array.
 
+def largestElement(arr):
+    largest = arr[0]
+
+    for num in arr:
+        if num > largest:
+            largest = num
+
+    return largest
+
+arr = list(map(int, input().split()))
+print(largestElement(arr))
