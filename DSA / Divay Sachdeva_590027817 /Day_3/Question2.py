e306453ae3a48012ac57b27f2cab4f4e6b21a8f0
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-17 23:22:58 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 /Day_3/Question2.py
+++ DSA / Divay Sachdeva_590027817 /Day_3/Question2.py
@@ -1,1 +1,15 @@
+# Given an sorted integer array and a target value, count the number of occurrences of the target in the array and return the result.
 
+def countOccurrences(arr, target):
+    count = 0
+
+    for num in arr:
+        if num == target:
+            count += 1
+
+    return count
+
+arr = list(map(int, input().split()))
+target = int(input())
+
+print(countOccurrences(arr, target))
