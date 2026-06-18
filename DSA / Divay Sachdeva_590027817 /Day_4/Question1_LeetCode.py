926cc57ec48281b7ae1e8e2493ea566f866df764
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-18 22:48:20 +0530

    Update Question1_LeetCode.py

--- DSA / Divay Sachdeva_590027817 /Day_4/Question1_LeetCode.py
+++ DSA / Divay Sachdeva_590027817 /Day_4/Question1_LeetCode.py
@@ -1,1 +1,16 @@
+# Given a 2D integer array matrix, return the transpose of matrix.
 
+#The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+class Solution(object):
+    def transpose(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        result = [[0] * rows for _ in range(cols)]
+
+        for i in range(rows):
+            for j in range(cols):
+                result[j][i] = matrix[i][j]
+
+        return result
