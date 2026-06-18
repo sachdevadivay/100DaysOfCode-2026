c4201c9822f9776045ef7d15ee702e024e72c53a
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-18 22:51:24 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 /Day_4/Question2.py
+++ DSA / Divay Sachdeva_590027817 /Day_4/Question2.py
@@ -1,1 +1,13 @@
+# Given an integer array and an integer k, rotate the array to the right by k steps.
 
+def rotateArray(nums, k):
+    n = len(nums)
+    k = k % n
+
+    nums = nums[-k:] + nums[:-k]
+    return nums
+
+nums = list(map(int, input().split()))
+k = int(input())
+
+print(rotateArray(nums, k))
