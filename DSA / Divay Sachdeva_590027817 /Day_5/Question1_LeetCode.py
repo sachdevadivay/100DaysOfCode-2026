328d36c43a1e49d8274b9d6a853c446103789924
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-19 23:10:34 +0530

    Update Question1_LeetCode.py

--- DSA / Divay Sachdeva_590027817 /Day_5/Question1_LeetCode.py
+++ DSA / Divay Sachdeva_590027817 /Day_5/Question1_LeetCode.py
@@ -1,1 +1,13 @@
+# You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
+class Solution(object):
+    def canJump(self, nums):
+        farthest = 0
+
+        for i in range(len(nums)):
+            if i > farthest:
+                return False
+
+            farthest = max(farthest, i + nums[i])
+
+        return True
