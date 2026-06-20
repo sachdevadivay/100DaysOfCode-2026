a4f47d89c45ebb49c37945d4f4e210b758be19da
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-20 23:53:07 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 /Day_6/Question2.py
+++ DSA / Divay Sachdeva_590027817 /Day_6/Question2.py
@@ -1,1 +1,8 @@
+# Given two sorted arrays, find the union of the arrays containing all distinct elements from both arrays.
 
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+union = sorted(set(arr1) | set(arr2))
+
+print(union)
