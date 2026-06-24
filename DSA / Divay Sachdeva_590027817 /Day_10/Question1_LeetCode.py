ce38eba36a07d254ff4f3b9de49c8fef4d260f4f
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-24 15:24:18 +0530

    Update Question1_LeetCode.py

--- DSA / Divay Sachdeva_590027817 /Day_10/Question1_LeetCode.py
+++ DSA / Divay Sachdeva_590027817 /Day_10/Question1_LeetCode.py
@@ -1,1 +1,5 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
+class Solution(object):
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
