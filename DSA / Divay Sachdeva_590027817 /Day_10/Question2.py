e44f22e5539657cd6e1ad0b6f2046947a6093dcb
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-24 15:26:29 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 /Day_10/Question2.py
+++ DSA / Divay Sachdeva_590027817 /Day_10/Question2.py
@@ -1,1 +1,10 @@
+# Count distinct characters in a string and print "CHAT WITH HER!" if even, "IGNORE HIM!" if odd.
 
+s = input()
+
+count = len(set(s))
+
+if count % 2 == 0:
+    print("CHAT WITH HER!")
+else:
+    print("IGNORE HIM!")
