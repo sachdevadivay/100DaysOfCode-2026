7e118f7dfec1790d7308bb887bd2225aff73fbd7
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-06-25 12:10:12 +0530

    Update Question2.py

--- DSA / Divay Sachdeva_590027817 /Day_11/Question2.py
+++ DSA / Divay Sachdeva_590027817 /Day_11/Question2.py
@@ -1,1 +1,12 @@
+# Modify a string by removing all vowels, converting consonants to lowercase, and adding a dot (.) before each consonant.
 
+s = input()
+
+vowels = "aeiouAEIOU"
+result = ""
+
+for ch in s:
+    if ch not in vowels:
+        result += "." + ch.lower()
+
+print(result)
